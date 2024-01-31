b4b387a0d41492bb0d0f4224346ab42631c7b7f3
Kiwizacxs <brian.deloen2016@outlook.com>
2024-01-31 01:09:16 +0000

    argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,11 @@
 import sys
 if __name__ == "__main__":
     i = 0
-    print("{} arguments:".format(len(sys.argv) - 1))
+    print("{} ".format(len(sys.argv) - 1), end="")
+    if len(sys.argv) == 2:
+        print("argument:")
+    elif len(sys.argv) > 2:
+        print("arguments:")
     for x in sys.argv:
         if x != sys.argv[0]:
             print("{}: {}".format(i, x))
