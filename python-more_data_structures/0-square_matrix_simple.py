fab6781d6638b739923c929deea4b2f984277ba9
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-03 03:28:53 +0000

    new matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    x = 0
-    new_matrix = []
-    while x != len(matrix):
-        new_matrix.append(list(map(lambda x: x * x, matrix[x])))
-        x += 1
-    return new_matrix
+    return [list(map(lambda x: x * x, y)) for y in matrix]
