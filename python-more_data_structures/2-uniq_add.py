496e69d42e6f1daf94db89ac7de670db8529f8f7
kiwizacxs <brian.deleon2016@outlook.com>
2024-02-03 23:19:21 +0000

    uniq add

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     sum_list = []
     x = 0
-    while x != len(my_list) - 1:
+    for x in my_list:
         if x not in sum_list:
             sum_list.append(x)
         x += 1
