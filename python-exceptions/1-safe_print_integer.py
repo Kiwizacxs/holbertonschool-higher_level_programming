9f819dab82602f3cccf6493878d9c48b4429a383
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-04 22:56:58 +0000

    safe print

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,9 +3,6 @@ def safe_print_integer(value):
     Error = 0
     try:
         print("{:d}".format(value))
-    except ValueError:
-        Error = 1
-    if Error == 1:
-        return False
-    else:
         return True
+    except ValueError, TypeError:
+        return False
