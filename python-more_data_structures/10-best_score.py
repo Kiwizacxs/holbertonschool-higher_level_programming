613e87befa346a2121e1787fb7f2b53880d5c645
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-04 17:48:38 +0000

    max integer in a dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return max(a_dictionary)
+    return max(a_dictionary, key=a_dictionary.get)
