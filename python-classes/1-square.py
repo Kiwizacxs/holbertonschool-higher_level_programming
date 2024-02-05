aaa1718844763bd47dc4e34e592636126644f2cf
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-05 00:26:44 +0000

    class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size):
+        """initialize size"""
+        self.__size = size
