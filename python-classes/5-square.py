cabaab4b3223a65bc54f20eafeacd671f5e7339b
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-05 00:48:21 +0000

    add print square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -28,7 +28,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     def my_print(self):
         """print an square"""
         if self.__size == 0:
