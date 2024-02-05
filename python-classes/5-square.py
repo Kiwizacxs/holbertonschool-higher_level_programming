6ebf0e824f407b8c316f675ac3f0f7ff2ad62a2a
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-05 00:47:40 +0000

    add print square

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """initialize size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """return area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def my_print(self):
+        """print an square"""
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print()
