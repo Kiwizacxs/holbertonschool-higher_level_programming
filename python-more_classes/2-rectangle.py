629804d13c3a71d4ed8984400cfe27db25b48776
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-07 15:07:58 +0000

    add area an perimeter methonds

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """class that define a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get width attr"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width attr"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """get height attr"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height attr"""
+        if not isinstance(value, int):
+            raise TypeError("heigth must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """return the area of the rectangle"""
+        return self.__height * self.__width
+    
+    def perimeter(self):
+        """return the area of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2 
