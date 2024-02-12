3500cc6e6f9ba8af734fa67ef7f0b11c49dd7233
Kiwizcaxs <brian.deleon2016@outlook.com>
2024-02-12 02:24:21 +0000

    my list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""module that contain the class MyList"""
+"""module that contain the class MyList
+"""
 
 
 class MyList(list):
-    """class that contain a function"""
+    """class that contain the
+    function print sorted
+    """
 
     def print_sorted(self):
-        """fnction that print a sorted list"""
+        """fnction that print a sorted list
+        """
         print(sorted(self))
