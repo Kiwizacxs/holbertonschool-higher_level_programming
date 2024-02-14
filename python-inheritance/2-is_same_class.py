5669839f26cc732e192c9db6137614b5d60b5f4e
Kiwizacxs <bbrian.deleon2016@outlook.com>
2024-02-14 14:18:09 +0000

    is same class?

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""module that contain a the
+function is_same_class
+"""
+
+
 def is_same_class(obj, a_class):
+    """function that check if an object
+    blongs to a class
+    """
     if type(obj) == a_class:
         return True
     else:
