743a858862353e424563df2cc5a7a13fedea3faf
Kiwizacxs <bbrian.deleon2016@outlook.com>
2024-02-14 15:16:40 +0000

    add method area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that contain te class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry that contain only a public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
