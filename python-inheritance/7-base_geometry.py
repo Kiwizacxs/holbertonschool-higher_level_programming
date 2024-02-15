1e92e993b4dd2e7e49d5abb09c8c86cd4d150f51
kiwizacxs <brian.deleon2016outlook.com>
2024-02-15 03:41:39 +0000

    update

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Module that contain the class BaseGeometry
-with all his functions
+"""
+Module that contain the class BaseGeometry
+with all his methods and functions for now.
 """
 
 
 class BaseGeometry:
-    """Class BaseGeometry that contain the methods:
+    """
+    Class BaseGeometry
+    Contain the methods:
     area() and integer_validator()
     """
     def area(self):
-        """function that are not implemented
+        """
+        function that are not implemented
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """function that valides that value is
+        """
+        function that valides that value is
         an integer
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
