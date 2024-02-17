0a51d76bfa42b4910b39eddc10016e2f3d5733cf
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-17 03:27:59 +0000

    createe the class rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Module that contain the class
+Rectangle with all his functions
+"""
+
+
+from .base import Base
+
+
+class Rectangle(Base):
+    """class rectangle with the private attributes:
+        __width -> width
+        __height -> height
+        __x -> x
+        __y -> y
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+ 
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value 
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = y
