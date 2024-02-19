2661e3fd926871d4fc1620030e0460e6857b460e
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-19 01:42:21 +0000

    update Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -47,3 +47,13 @@ class Base:
                 for obj in list_objs:
                     _dict.append(obj.to_dictionary())
                 f.write(Base.to_json_string(_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        static method hat returns the list of the JSON string
+        representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
