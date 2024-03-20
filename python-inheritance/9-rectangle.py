2d5b0eab1e30f55db0eafc19928220e4299e7488
Kiwizacxs <brian.deleon2016@outlook.com>
2024-03-20 14:28:20 +0000

    full rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Module that contain the class BaseGeometry
+with all his methods and functions for now.
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+
+    Contain the methods:
+    - area()
+    - integer_validator()
+    """
+    def area(self):
+        """
+        function that are not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function that valides that value is
+        an integer
+        raise an Error if value is not int and
+        raise an Error if value is less or equal 0
+        Name aways are a string.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    Clas that create a rectangle
+    """
+    def __init__(self, height, width):
+        """
+        class constructor
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+    
+    def area(self):
+        """Method that return the area of the trectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """method that return a string"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
