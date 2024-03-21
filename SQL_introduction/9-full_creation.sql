5c136166eabd38c33ea34a2682a8b2dcf6f913f0
Kiwizacxs <brian.deleon2016@outlook.com>
2024-03-21 16:37:53 +0000

    full table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create full table if this not exists
-CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table (id, name, score)
 VALUES
