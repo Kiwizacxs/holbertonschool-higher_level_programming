012701051f953fb95bc93eb5a1c80f73c009e8db
Kiwizacxs <brian.deleon2016@outlook.com>
2024-03-21 16:38:46 +0000

    full table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
--- Create full table if this not exists
+-- Create a table second_table full if these not exists
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
-INSERT INTO second_table (id, name, score)
+INSERT INTO second_table(id, name, score)
 VALUES
-(1, 'John', 10)
+(1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
 (4, "George", 8);
